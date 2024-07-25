278ee6bd7b08d2ba4c96596d0ee310eaf65d748d
Maham Tariq <156241209+maham-tariq5@users.noreply.github.com>
2024-07-24 21:06:36 -0400

    Update Note.java

--- Note.java
+++ Note.java
@@ -1,20 +1,25 @@
-public class Note {
-    private int string;
-    private int fret;
 
+public class Note {
+    private int string;  
+    private int fret;    
+
+    // Constructor to initialize a Note object with the given string and fret
     public Note(int string, int fret) {
-        this.string = string;
-        this.fret = fret;
+        this.string = string;  // Set the string number
+        this.fret = fret;      // Set the fret number
     }
 
+    // Getter method to retrieve the string number
     public int getString() {
         return string;
     }
 
+    // Getter method to retrieve the fret number
     public int getFret() {
         return fret;
     }
 
+    // Override the toString method to provide a string representation of the Note
     @Override
     public String toString() {
         return "String: " + string + ", Fret: " + fret;
